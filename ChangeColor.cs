80a47c1886a1d6561bd8cf46a1e089335984fb15
Dandikur <dandikur@gmail.com>
2021-01-05 11:39:28 +0700

    Add files via upload

---
+++ ChangeColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public GameObject targete;
+    public string milihe;
+
+    void OnMouseUp()
+    {
+        if(milihe == "1")
+        {
+            targete.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+        }
+        else if(milihe == "2")
+        {
+            targete.GetComponent<Renderer>().material.color = new Color(255, 198, 198);
+        }
+        else if(milihe == "3")
+        {
+            targete.GetComponent<Renderer>().material.color = new Color(253, 120, 120);
+        }
+        else if (milihe == "4")
+        {
+            targete.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+        }
+    }
+}
---
+++ CobaPrint.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sfs2X.Entities.Data;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEditor;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+
+public class CobaPrint : MonoBehaviour
+{
+	public GameObject cobaMarker;
+	
+    string path = null;
+    
+    void Start()
+    {
+        path = Application.dataPath + "/Marker.pdf";  
+       
+    }
+
+    public void GenerateFile() {
+        if (File.Exists(path))
+            File.Delete(path);
+        using (var fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+        {
+            var document = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+            var writer = PdfWriter.GetInstance(document, fileStream);
+
+            document.Open();
+
+            document.NewPage();
+          
+            document.Add(cobaMarker);
+
+            document.Close();
+            writer.Close();
+        }
+        PrintFiles();
+    }
+
+    void PrintFiles()
+    {
+        Debug.Log(path);
+        if (path == null)
+            return;
+
+        if (File.Exists(path))
+        {
+            Debug.Log("file found");
+        }
+        else
+        {
+            Debug.Log("file not found");
+            return;
+        }
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+        process.StartInfo.UseShellExecute = true;
+        process.StartInfo.FileName = path;
+
+        process.Start();
+    }
+}
